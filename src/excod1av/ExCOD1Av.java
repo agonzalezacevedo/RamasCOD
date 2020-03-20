7373c501a5c68232da4eab7c5ce20a14cddb7bee
agonzalezacevedo <agonzalezacevedo@danielcastelao.org>
2020-03-20 01:21:40 +0100

    Modificación E cambio de rama

--- src/excod1av/ExCOD1Av.java
+++ src/excod1av/ExCOD1Av.java
@@ -15,7 +15,7 @@ public class ExCOD1Av {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Modificación B en la rama principal");
+        System.out.println("Modificación E cambio a la rama principal");
     }
     
 }
